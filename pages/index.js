bbe7c6d0d51911d64bc032326116a7662626a4a1
AdamWilson880 <65422010+AdamWilson880@users.noreply.github.com>
2022-04-26 22:38:05 -0500

    Create Individual Movie Page
    and made page url dynamic

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   useEffect(() => {}, []);
   return AuthCheck(
     <MainLayout>
-      <FeaturedMedia />
+      <FeaturedMedia videoUrl="https://www.youtube.com/embed/NYH2sLid0Zc?autoplay=1&loop=1&start=16" title='Mortal Kombat' location='text' mediaUrl='/movies/id'/>
       <LazyLoad
         offset={-400}
         placeholder={<PlaceHolders title="Movies" type="large-v" />}>
