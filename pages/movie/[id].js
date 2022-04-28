9ff671e45a73d09e707c4270f0941e41800296a7
AdamWilson880 <65422010+AdamWilson880@users.noreply.github.com>
2022-04-27 22:18:48 -0500

    Linked Thumbnals
    to correct movie pages

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,45 +1,51 @@
-import Head from 'next/head'
-import {useEffect, useState} from 'react'
-import AuthCheck from '../../components/AuthCheck'
-import MainLayout from'../../components/Layouts/MainLayout'
-import CastInfo from'../../components/UI/CastInfo/CastInfo'
-import FeaturedMedia from '../../components/UI/FeaturedMedia/FeaturedMedia'
-import MediaRow from '../../components/UI/MediaRow/MediaRow'
-import { useRouter } from 'next/router'
-import axios from 'axios'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import AuthCheck from "../../components/AuthCheck";
+import MainLayout from "../../components/Layouts/MainLayout";
+import CastInfo from "../../components/UI/CastInfo/CastInfo";
+import FeaturedMedia from "../../components/UI/FeaturedMedia/FeaturedMedia";
+import MediaRow from "../../components/UI/MediaRow/MediaRow";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function SingleMediaPage(props) {
-  const router = useRouter()
+  const router = useRouter();
   const [mediaData, setMediaData] = useState(false);
   // const { id } = router.query
-  console.log(props)
+  console.log(props);
   useEffect(() => {
     axios
-      .get(`https://api.themoviedb.org/3/movie/${props.query.id}?api_key=f40103be50cc9b93a331d8f0f2eeb811&language=en-US`)
+      .get(
+        `https://api.themoviedb.org/3/movie/${props.query.id}?api_key=f40103be50cc9b93a331d8f0f2eeb811&language=en-US`
+      )
       .then(function (response) {
-        setMediaData(response.data)
+        setMediaData(response.data);
         console.log(response);
       })
       .catch(function (error) {
         // handle error
-        console.log('Error Response For ');
+        console.log("Error Response For ");
         console.log(error);
-      }) 
+      });
   }, []);
 
-  
   return AuthCheck(
     <MainLayout>
-      <h1 style={{color: 'white'}}>${props.query.id}</h1>
-      <FeaturedMedia title={mediaData.title} />
+      <FeaturedMedia
+        title={mediaData.title}
+        mediaUrl={`https://image.tmdb.org/t/p/w1280${mediaData.backdrop_path}`}
+        location="text"
+        linkUrl="/movies/id"
+        type="single"
+      />
       {/* <MediaRow title="More Like This" type="small-v" /> */}
       <CastInfo />
     </MainLayout>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
   return {
-    props: {query: context.query}, // will be passed to the page component as props
-  }
+    props: { query: context.query }, // will be passed to the page component as props
+  };
 }
